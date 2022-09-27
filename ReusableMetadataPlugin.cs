c6a8e985f450d1e7af32c47d483b0729dfd56fb8
Matthew Howell <mrhowel@g.clemson.edu>
2022-09-26 21:53:12 -0400

    Bump to version 1.1.0. WIP

--- ReusableMetadataPlugin.cs
+++ ReusableMetadataPlugin.cs
@@ -17,7 +17,7 @@ namespace ReusableMetadata
     {
         public const string pluginGuid = "11matt556.dysonsphereprogram.ReusableMetadata";
         public const string pluginName = "Reusable Metadata";
-        public const string pluginVersion = "1.0.6";
+        public const string pluginVersion = "1.1.0";
 
         public static ConfigEntry<bool> useHighestProductionOnly;
         public static ConfigEntry<bool> useVerboseLogging;
